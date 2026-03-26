7a5c8afe4ab51d018e88b3516660b3778018378b
nicolasr-art <nicolasr@consultoresc.com.ar>
2026-03-26 15:56:33 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,30 @@
 
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("message", async (event) => {
+  const data = event.data || {};
+
+  if (data.type === "MOSTRAR_NOTIFICACION") {
+    await self.registration.showNotification("Avisos", {
+      body: data.body || "Tenés un recordatorio",
+    });
+  }
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      return clients.openWindow("/avisos-app/");
+    })
+  );
+});
