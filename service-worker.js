baadb48a8367168c406fccedc9c31a86eff522db
nicolasr-art <nicolasr@consultoresc.com.ar>
2026-03-27 11:04:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,7 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+  console.log('Service Worker instalado');
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("message", async (event) => {
-  if (event.data?.type === "MOSTRAR_NOTIFICACION") {
-    await self.registration.showNotification("Avisos", {
-      body: event.data.body || "Recordatorio",
-    });
-  }
-});
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-
-  event.waitUntil(
-    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
-      if (clientList.length > 0) {
-        return clientList[0].focus();
-      }
-      return clients.openWindow("/");
-    })
-  );
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activo');
 });
